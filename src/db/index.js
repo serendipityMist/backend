d0fa924509e5ad3317980e1387f2420abb4b805b
serendipityMist <serendipityMist@gmail.com>
2025-01-13 19:00:35 +0545

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,18 @@
 import mongoose, { mongo } from "mongoose";
 import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
+
+    // app.on("error", (error) => {
+    //   console.log(`GEtting ERRORRRRR!!!!!!!!`, error);
+    // });
     console.log(
-      `/n MONGOSB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`
+      `/n MONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("MONGODB CONNECTION FAILED: ", error);
---
+++ src/models/video.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new mongoose.Schema(
+  {
+    videFile: {
+      type: string, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: string, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: string,
+      required: true,
+    },
+    description: {
+      type: string,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinary
+      rquired: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
