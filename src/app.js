dea364e19ca4e8490996f148ea85fce449a23016
serendipityMist <serendipityMist@gmail.com>
2025-01-17 18:52:46 +0545

    register route

--- src/app.js
+++ src/app.js
@@ -23,4 +23,10 @@ app.use(
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+import router from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", router);
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,21 @@
 //Using Promise
-// const asyncHandler = (requestHandler) => {
-//   (req, res, next) => {
-//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 //Using Try Catch
 
-const asyncHandler = (fn = async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
+// const asyncHandler = (fn = async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// });
 
 export { asyncHandler };
